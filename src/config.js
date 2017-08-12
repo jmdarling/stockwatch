8ee1524d5824a84f5d63bcaf6901342cc6468677
Jonathan Darling <jmdarling@me.com>
2017-08-12 01:21:21 -0500

    Fixes dumb typo.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   alphavantageApiKey: process.env.ALPHAVANTAGE_API_KEY,
-  port: process.env.PORT | 8080,
-  redisUrl: process.env.REDIS_URL | '//localhost:6379'
+  port: process.env.PORT || 8080,
+  redisUrl: process.env.REDIS_URL || '//localhost:6379'
 }
